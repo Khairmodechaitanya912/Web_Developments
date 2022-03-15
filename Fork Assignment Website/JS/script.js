8a413a1d10c7091180f6ab676e8978e18bac44d4
Chaitanya Khairmode <chaitanyakhairmode912@gmail.com>
2022-03-15 19:21:13 +0530

    Fork Assignment Website

---
+++ Fork Assignment Website/JS/script.js
@@ -0,0 +1,8 @@
+function getdata(){
+    const Name= document.getElementById("Name").value;
+    const Collage= document.getElementById("CName").value;
+    const Email= document.getElementById("email").value;
+    alert(`Name :-${Name}
+Collage :-${Collage} 
+Email :-${Email}`);
+}
